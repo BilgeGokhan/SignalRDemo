7c4a81589781190e0a7847c83c414aa7a8ea8a9c
BilgeGokhan <47634641+BilgeGokhan@users.noreply.github.com>
2020-05-26 16:48:43 +0300

    Initial commit

---
+++ SignalRDemo/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRDemo.App_Start.Startup))]
+
+namespace SignalRDemo.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRDemo.Hubs
+{   
+    public class ChatHub : Hub
+    {    
+        [HubMethodName("sendMessage")]
+        public void SendMessage(string name, string message)
+        {
+            Clients.Others.GetMessageOther(name, message);
+
+            Clients.Caller.GetMessageCaller(message);
+        }
+    }
+}
